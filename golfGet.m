1e1d08a599ca56ec5610a95af25471f7c4be27d3
misterboyle <mrboyle@live.unc.edu>
2015-12-07 20:05:39 -0500

    first commit, skeleton .m file for golf ball detection created

---
+++ golfGet.m
@@ -0,0 +1,14 @@
+%% Golf-get
+% Authors: Michael Boyle
+% Jim Redden
+% Date created: 12/07/2015
+% 
+% Description: Reads in .mov and exports image-relative coordinates of golf ball center
+%
+%
+%
+%
+%
+function ballPos = golfGet(myImg)
+	%use VideoReader function to load in .mov file, use spatial Laplacian to detect elipses (golf balls) in images
+end
